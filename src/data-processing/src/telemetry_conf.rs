110fa577a78b8d3169fe4b5dec41736b6c2dfd70
Wasim Alayoubi <14363692+wayoubi@users.noreply.github.com>
2025-10-27 15:15:50 -0400

    Update telemetry_conf.rs

--- src/data-processing/src/telemetry_conf.rs
+++ src/data-processing/src/telemetry_conf.rs
@@ -2,36 +2,23 @@ use opentelemetry::global;
 use opentelemetry_otlp;
 use opentelemetry_sdk::{
     propagation::TraceContextPropagator,
-    resource::{
-        EnvResourceDetector, OsResourceDetector, ProcessResourceDetector, ResourceDetector,
-        SdkProvidedResourceDetector, TelemetryResourceDetector,
-    },
-    runtime, trace as sdktrace, Resource,
+    resource::{EnvResourceDetector, Resource},
+    runtime, trace as sdktrace,
 };
-
 use std::time::Duration;
 
 fn get_resource_attr() -> Resource {
-    let os_resource = OsResourceDetector.detect(Duration::from_secs(0));
-    let process_resource = ProcessResourceDetector.detect(Duration::from_secs(0));
-    let sdk_resource = SdkProvidedResourceDetector.detect(Duration::from_secs(0));
-    let env_resource = EnvResourceDetector::new().detect(Duration::from_secs(0));
-    let telemetry_resource = TelemetryResourceDetector.detect(Duration::from_secs(0));
-
-    os_resource
-        .merge(&process_resource)
-        .merge(&sdk_resource)
-        .merge(&env_resource)
-        .merge(&telemetry_resource)
+    let env_resource = EnvResourceDetector::new().detect();
+    env_resource
 }
 
-pub fn init_tracer() -> () {
+pub fn init_tracer() {
     global::set_text_map_propagator(TraceContextPropagator::new());
 
-    opentelemetry_otlp::new_pipeline()
-        .tracing()
-        .with_exporter(opentelemetry_otlp::new_exporter().tonic())
-        .with_trace_config(sdktrace::config().with_resource(get_resource_attr()))
+    opentelemetry_otlp::new_exporter()
+        .tonic()
+        .build_trace_pipeline()
+        .with_trace_config(sdktrace::Config::default().with_resource(get_resource_attr()))
         .install_batch(runtime::Tokio)
         .expect("pipeline install error");
 }
